8069511f5ab08d22587a2b7051192328a78dbef9
aizhen1412 <2662923241@qq.com>
2023-09-01 22:41:16 +0800

    2023/9/1 22:41

--- token.h
+++ token.h
@@ -3,24 +3,25 @@
 
 #include "token_type.h"
 #include <string>
+#include <any>
 
 class Token // 词法单元
 {
 public:
-    Token(TokenType type, std::string lexeme, std::string literal, int line)
+    Token(TokenType type, std::string lexeme, std::any literal, int line)
         : type(type), lexeme(lexeme), literal(literal), line(line)
     {
         // 构造函数体为空，因为成员变量已经在初始化列表中初始化
     }
     std::string ToString() // 将词法单元转换为字符串
     {
-        return type + " " + lexeme + " " + literal;
+        return type + " " + lexeme + " " + std::any_cast<std::string>(literal);
     }
 
 private:
     const TokenType type;
     const std::string lexeme;
-    const std::string literal; // 词法单元的字面量
+    const std::any literal; // 词法单元的字面量
     const int line;
 };
 #endif // TOKEN_H
